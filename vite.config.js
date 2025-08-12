64f7e9c5f2e67e62e17b33ee8615062dff726194
Adarsh1442005 <btech60077.23@gmail.com>
2025-08-13 00:22:50 +0530

    distadded

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // ✅ binds to 0.0.0.0
     allowedHosts: ['swarfrontend.onrender.com'], // ✅ allow your Render domain
+  },
+  build: {
+    outDir: 'dist', // ✅ ensures Render knows where to find static files
   }
+
 
 })
