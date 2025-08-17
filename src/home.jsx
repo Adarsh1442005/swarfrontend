25e9e9797cd791971c52e39f01e51b2f5d4b0ef3
Adarsh1442005 <btech60077.23@gmail.com>
2025-08-17 15:09:57 +0530

    deletemono

--- src/home.jsx
+++ src/home.jsx
@@ -9,10 +9,10 @@ import mono from './mono.jpg';
 const presidents = [
   { name: 'Adarsh Pandey', role: 'Technical Lead, Instrumentalist & Vocalist', image: pic3 },
   { name: 'Riya', role: 'Vocalist & Event Curator' ,image:pic4 },
-  { name: 'Abhinav', role: 'Instrumentalist & Outreach Coordinator', image: pic },
+  { name: 'Abhinav', role: 'Instrumentalist(Guitar) & Outreach Coordinator', image: pic },
   { name: 'Avani', role: 'Vocalist & Event Curator', image: pic2 },
   { name: 'Navonil', role: 'Vocalist, Community Builder & Performer',image:navonil },
-  {nmae:'Monoshwini',role:"Instrumentalist(first feamle Guitarist of Swar),Community build",image:mono}
+  
 ];
 
 export function SwarHomePage() {
