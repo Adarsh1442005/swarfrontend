75b977aa00c372f84d9cb76274bc0bf430ce8bff
Adarsh Pandey <btech60077.23@bitmesra.ac.in>
2026-01-18 22:35:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { Routes, Route,Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MembershipForm } from './member.jsx';
 import { SwarHomePage } from './home.jsx';
 import { MusicSubmit } from './Musicsubmission.jsx';
 import { AudioPlayer } from './listenaudio.jsx';
 import { VideoGallery } from './perfiormaces.jsx';
-import {MessagesTable} from './data.jsx';
+import { MessagesTable } from './data.jsx';
 import { AdminLogin } from './adminpage.jsx';
 import { Audioadmin } from './audioadminlogin.jsx';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SwarHomePage />} />
-      <Route path="/membership" element={<MembershipForm />} />
-      <Route path="/music" element={<MusicSubmit />}/> 
-      <Route path="/listen" element={<AudioPlayer />} />
-      <Route path="/perform" element={<VideoGallery />}/>
-      <Route path="/data" element={<MessagesTable/>}/>
-      <Route path="/admin" element={<AdminLogin/>}/>
-      <Route path="/adminb" element={<Audioadmin/>}/>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SwarHomePage />} />
+        <Route path="/membership" element={<MembershipForm />} />
+        <Route path="/music" element={<MusicSubmit />} /> 
+        <Route path="/listen" element={<AudioPlayer />} />
+        <Route path="/perform" element={<VideoGallery />} />
+        <Route path="/data" element={<MessagesTable />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/adminb" element={<Audioadmin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
